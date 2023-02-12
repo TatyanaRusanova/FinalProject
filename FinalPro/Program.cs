10104fb5cf622fdf570e8285247652dcceea2083
Tatyana <kta995@mail.ru>
2023-02-12 19:10:04 +0300

    NewArr

--- FinalPro/Program.cs
+++ FinalPro/Program.cs
@@ -8,5 +8,17 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”] 
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string[] array1=new string[5] {"125", "88", "array", "Hi", "Hello"};
-string[] array2= new string[array1.Length];
+Console.WriteLine("Введите количество элементов массива: ");
+int size  = int.Parse(Console.ReadLine());
+
+string[] array1 = new string[size];
+
+for (int i = 0; i < size; i++){
+	Console.Write("Введите элемент массива: ");
+    string result = Console.ReadLine();
+	array1[i] = result;  
+}
+Console.WriteLine();
+Console.Write("Введенный массив: [");
+Console.Write(string.Join(",", array1));
+Console.Write("]");
